f168608f26ff7f346f6a5f84fcc6f1a84b9de8ea
Guy Coleman <coleman.guyg@gmail.com>
2021-12-31 16:04:20 +0200

    .

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -6,13 +6,25 @@ public class Mover : MonoBehaviour
 
     void Start()
     {
-
+        PrintInstructions();
     }
 
     void Update()
     {
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
         float xValue = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
         float zValue = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         transform.Translate(xValue, 0, zValue);
+    }
+
+    void PrintInstructions()
+    {
+        Debug.Log("Welcome to the game");
+        Debug.Log("Move your player with 'wasd' or arrow keys.");
+        Debug.Log("Dodge everything!");
     }
 }
